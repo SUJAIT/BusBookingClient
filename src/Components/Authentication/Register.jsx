e55467ed53db5ff5c394c312f0d1d9806f42c3eb
SUJAIT <sujait2000@.com>
2024-10-23 23:29:50 +0600

    added in register firbase api

--- src/Components/Authentication/Register.jsx
+++ src/Components/Authentication/Register.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import './Register.css'
+import { AuthContext } from './ContextApi/AuthProvider'
 
 const Register = () => {
+
+  const {createUser} = useContext(AuthContext)
+  const navigate = useNavigate()
+  const location = useLocation()
+  const from = location.state?.from?.pathname||'/';
+
+  // user create function 
+  const userCreateHandler = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user = {email,password,name}
+    console.log(user)
+
+    // firbase api call 
+    createUser(email,password)
+    .then((userCredential) =>{
+      //signin
+      const user = userCredential.user;
+      //
+      console.log(user)
+      form.reset()
+      navigate(navigate(from,{replace:true}))
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode,errorMessage)
+      // ..
+    });
+          //  firbase working End
+  
+
+  }
+
   return (
     <div>
  
@@ -18,30 +56,30 @@ const Register = () => {
   <p className='mt-4 text-2xl font-semibold'>Register in to Jatri</p>
 </div>
 
-      <form className="card-body ">
+      <form onSubmit={userCreateHandler} className="card-body ">
         <div className="form-control">
           <label className="label">
             <span className="label-text">User</span>
           </label>
-          <input type="email" placeholder="User Name" className="input input-bordered" required />
+          <input type="name" name='name' placeholder="User Name" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input name='password' type="password" placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <Link className="btn btn-error"><span className='text-white font-bold'>Sign-in</span></Link>
+          <input className="btn btn-error aab" type="submit" value="Sign-in"/>
         </div>
      <span className='text-lg font-semibold'><span className='fontColor mr-2'>Already have an account?</span><Link className='text-sky-500' to={'/login'}>Login</Link></span>   
       </form>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './App.css'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Components/Routers/Router.jsx'
+import AuthProvider from './Components/Authentication/ContextApi/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
-<RouterProvider router={router}/>
+  <AuthProvider>
+   <RouterProvider router={router}/> 
+  </AuthProvider>
+
 
   </StrictMode>,
 )
